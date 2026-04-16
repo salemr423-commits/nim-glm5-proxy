9a3c1ea96240767e3e60160dfb4fc61e81618e4d
salemr423-commits <salemr423@gmail.com>
2026-04-16 18:04:52 +0200

    Update route.js

--- app/api/v1/chat/completions/route.js
+++ app/api/v1/chat/completions/route.js
@@ -12,7 +12,7 @@ export async function OPTIONS() {
 export async function POST(req) {
   try {
     const body = await req.json();
-    body.model = "deepseek-ai/deepseek-v3_1";
+    body.model = "deepseek-ai/deepseek-v3_2";
 
     const apiKey = process.env.NVIDIA_NIM_API_KEY;
 
